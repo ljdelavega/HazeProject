709573ac4899862884c0869f976d7217eee556c2
Lester Dela Vega <lester.delavega@gmail.com>
2016-03-30 14:48:05 -0600

    Finishing the remaining create table commands.

--- createtables.php
+++ createtables.php
@@ -18,8 +18,8 @@ if ($conn->connect_error) {
 
 // sql to create User table if it doesn't already exist
 $sql = "CREATE TABLE IF NOT EXISTS User (
-username VARCHAR(20) NOT NULL,
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(30) NOT NULL UNIQUE,
+list_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 password VARCHAR(20) NOT NULL,
 firstname VARCHAR(30) NOT NULL,
 lastname VARCHAR(30) NOT NULL,
@@ -29,10 +29,84 @@ reg_date TIMESTAMP
 
 if ($conn->query($sql) === TRUE) {
     echo "Table User created successfully";
+    echo "\r\n";
 } else {
-    echo "Error creating table: " . $conn->error;
+    echo "Error creating table User: " . $conn->error;
+    echo "\r\n";
 }
 
+// sql to create Game table if it doesn't already exist
+$sql = "CREATE TABLE IF NOT EXISTS Game (
+game_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+price DECIMAL(10,2) NOT NULL,
+genre VARCHAR(30) NOT NULL
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table Game created successfully";
+    echo "\r\n";
+} else {
+    echo "Error creating table Game: " . $conn->error;
+    echo "\r\n";
+}
+
+// sql to create Contains table if it doesn't already exist
+$sql = "CREATE TABLE IF NOT EXISTS Contains (
+list_id INT(6) UNSIGNED,
+game_id INT(6) UNSIGNED,
+PRIMARY KEY (list_id, game_id),
+FOREIGN KEY (list_id) REFERENCES User(list_id),
+FOREIGN KEY (game_id) REFERENCES Game(game_id)
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table Contains created successfully";
+    echo "\r\n";
+} else {
+    echo "Error creating table Contains: " . $conn->error;
+    echo "\r\n";
+}
+
+// sql to create Reviews table if it doesn't already exist
+$sql = "CREATE TABLE IF NOT EXISTS Reviews (
+username VARCHAR(30) NOT NULL UNIQUE,
+game_id INT(6) UNSIGNED,
+rating INT(1) UNSIGNED,
+text_review TEXT,
+PRIMARY KEY (username, game_id),
+FOREIGN KEY (username) REFERENCES User(username),
+FOREIGN KEY (game_id) REFERENCES Game(game_id)
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table Reviews created successfully";
+    echo "\r\n";
+} else {
+    echo "Error creating table Reviews: " . $conn->error;
+    echo "\r\n";
+}
+
+// sql to create CompletionState table if it doesn't already exist
+$sql = "CREATE TABLE IF NOT EXISTS CompletionState (
+list_id INT(6) UNSIGNED,
+game_id INT(6) UNSIGNED,
+hours INT(8) UNSIGNED,
+percent_complete DECIMAL(2,1),
+date_completed DATETIME,
+state VARCHAR(30),
+PRIMARY KEY (list_id, game_id),
+FOREIGN KEY (list_id) REFERENCES User(list_id),
+FOREIGN KEY (game_id) REFERENCES Game(game_id)
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table CompletionState created successfully";
+    echo "\r\n";
+} else {
+    echo "Error creating table CompletionState: " . $conn->error;
+    echo "\r\n";
+}
 
 //mysql_close($conn);
 $conn->close();
