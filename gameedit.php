8197aa1c2f96ce4b19bf66375789e0e30e7b56eb
KyleMa <kyletma@gmail.com>
2016-04-23 13:16:08 -0600

    Gameedit & gameedit stuff added to site. Delete function added as well but not implemented.

--- gameedit.php
+++ gameedit.php
@@ -6,6 +6,18 @@ $page_keywords = array("game collection", "video game tracker", "track games", "
 /* Note: Always load the config file for each page */
 require_once("resources/config.php");
 require_once(TEMPLATES_PATH . "/header.php");
+
+
+if(isset($_POST['submitted']))
+{
+	if ($site->UpdateGame())
+	{
+		echo "Game successfully updated! \r\n";
+			$site->RedirectToURL("main.php");
+	}
+}
+
+
 ?>
 
 <!-- Insert content here -->
@@ -13,18 +25,19 @@ require_once(TEMPLATES_PATH . "/header.php");
 	<div class="row">
     <div class="col-xs-6">
       <h1>Edit game in your game list</h1>
-      <form>
+      <form id='update' action='<?php echo $site->GetSelfScript(); ?>' method='post' accept-charset='UTF-8'>
+				<input type='hidden' name='submitted' id='submitted' value='1'/>
 				<div class="form-group">
 					<label for="game_name">Game Name *</label>
-					<input type="text" id="game_name" class="form-control" placeholder="e.g. Super Mario Bros." required autofocus autocomplete>
+					<input type="text" id="game_name" name="game_name" class="form-control" placeholder="e.g. Super Mario Bros." required autofocus autocomplete>
 				</div>
 				<div class="form-group">
 					<label for="genre">Genre *</label>
-					<input type="text" id="genre" class="form-control" placeholder="e.g. First-person shooter" required autofocus autocomplete>
+					<input type="text" id="genre" name="genre" class="form-control" placeholder="e.g. First-person shooter" required autofocus autocomplete>
 				</div>
 				<div class="form-group">
-					<label for="hours_played">Hours played *</label>
-					<input type="text" id="hours_played" class="form-control" placeholder="e.g. 23" required autofocus autocomplete>
+					<label for="Price">Price *</label>
+					<input type="text" id="price" name="price" class="form-control" placeholder="e.g. 23" required autofocus autocomplete>
 				</div>
 				<div class="form-group">
 					<label for="completion_state">Completion state *</label>
@@ -42,7 +55,7 @@ require_once(TEMPLATES_PATH . "/header.php");
           </div>
 				</div>
 
-				<input type="submit" name="signup" class="btn btn-primary" value="Save edits">
+				<input type="submit" name="Submit" class="btn btn-primary" value="Save edits">
         <a class="btn btn-default" href="main.php" role="button">Back to Game List</a>
 			</form>
 
