a886bb69b5896e1acf5fec570285a7772bfe7234
Lester Dela Vega <lester.delavega@gmail.com>
2016-04-24 10:39:35 -0600

    Added analytics page with correct calculations.

--- analytics.php
+++ analytics.php
@@ -19,45 +19,89 @@ $username = $_SESSION['username'];
 $firstname = $_SESSION['firstname'];
 $lastname = $_SESSION['lastname'];
 $list_id = $_SESSION['list_id'];
+
+// populate the game list with user's list of games.
+$gamelist = $site->GetGamesFromList($list_id);
+$total_price = 0;
+$total_hours = 0;
+$total_price_vs_hours = 0;
+if (!$gamelist)
+{
+  echo "You currently have no games in your list.";
+}
+else
+{
+  //loop through to get total price and total hours
+   while ($row = mysqli_fetch_array($gamelist)) {
+       $total_price += $row['price'];
+       // now get hours from the completion state for the game.
+       $completion_state_result = $site->GetCompletionStateByID($row['game_id']);
+       if (!$completion_state_result)
+       {
+       	echo "There is no completion state with this ID: " . $game_id ;
+       }
+       $completion_state = mysqli_fetch_array($completion_state_result);
+       $total_hours += $completion_state['hours'];
+     }
+}
+$total_price_vs_hours = round (($total_price / $total_hours), 2);
 ?>
 
 <!-- Insert content here -->
 <div class = "container-fluid">
 	<div class="row">
 		<div class="col-xs-12 bg-primary">
-			<h1>Welcome <?php echo $username;?>!</h1>
-      <p><?php echo $firstname;?> <?php echo $lastname;?></p>
+			<h1>Analytics Overview for: <?php echo $username;?>!</h1>
 		</div>
     <div class="col-xs-12">
-			<a href="gameadd.php" class="btn btn-primary">Add Game</a>
+      <h1>Overall Stats</h1>
+      <div class="row">
+        <div class="well col-xs-4">
+          <h3>Total Hours Played</h3>
+          <p><?php echo $total_hours ?></p>
+        </div>
+        <div class="well col-xs-4">
+          <h3>Total Money Spent</h3>
+          <p><?php echo $total_price ?></p>
+        </div>
+        <div class="well col-xs-4">
+          <h3>Average Price Per Hour</h3>
+          <p><?php echo $total_price_vs_hours ?></p>
+        </div>
+      </div>
 		</div>
     <div class="col-xs-12">
-      <h1>Your Game List</h1>
+      <h1>Individual Game Stats</h1>
       <table class="table table-striped">
 				<tr>
           <th>Game</th>
 					<th>Price</th>
-          <th>Genre</th>
-					<th>Actions</th>
+					<th>Hours</th>
+          <th>Price Per Hour</th>
         </tr>
 				<?php
-					// populate the game list with user's list of games.
-					$gamelist = $site->GetGamesFromList($list_id);
+          // populate the game list with user's list of games.
+          $gamelist = $site->GetGamesFromList($list_id);
 					if (!$gamelist)
 					{
 						echo "You currently have no games in your list.";
 					}
 					else
 					{
-	           while ($row = mysqli_fetch_array($gamelist)) {?>
+	           while ($row = mysqli_fetch_array($gamelist)) {
+               // now get hours from the completion state for the game.
+               $completion_state_result = $site->GetCompletionStateByID($row['game_id']);
+               if (!$completion_state_result)
+               {
+               	echo "There is no completion state with this ID: " . $game_id ;
+               }
+               $completion_state = mysqli_fetch_array($completion_state_result);
+               ?>
 	               <tr>
 	               <td><?php echo $row['game_name'];?></td>
 	               <td><?php echo $row['price'];?></td>
-	               <td><?php echo $row['genre'];?></td>
-	               <td>
-									 <a class="btn btn-primary" href="gameedit.php<?php $_SESSION['game_id'] = $row['game_id'];?>">Edit</a>
-		             	 <button class="btn">Review</button>
-							 	 </td>
+	               <td><?php echo $completion_state['hours'];?></td>
+                 <td><?php echo round(($row['price'] / $completion_state['hours']), 2);?></td>
 	               </tr>
 	  	<?php }
 					} ?>
